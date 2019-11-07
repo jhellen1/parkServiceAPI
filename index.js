79476a1a0156f565518fd87d520de9faf5329aeb
Justin Hellen <jmhellen1@gmail.com>
2019-11-06 19:17:43 -0500

    further functionality updates, commented out corrections that need fixing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const apiKey = '8Q6v8cdM36qsZgykhyK2fBfLjidC0ya7LzDK0lPa';
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
+let states = [ "AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"];
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -43,11 +44,26 @@ function getParksList(state, limit=10) {
 }
 
 function watchForm() {
+    $('#js-search-term').on('change',function(){
+        $('#stateError').text("")
+        let noWhiteSpace = $(this).val().replace(/\s+/g, '');
+        $(this).val(noWhiteSpace)
+        let tempArr = $(this).val().split(",")
+        let errorArr = tempArr.filter(item => {
+            // if (states.indexOf(item.toUpperCase()) === -1) {
+            //     return(item);
+            // }
+        })
+        $('#stateError').text("The following isn't a state: " + errorArr.join(","));
+    })
     $('form').submit(event => {
         event.preventDefault();
-        const state = $('#js-search-term').val();
+        const state = $('#js-search-term').val().split(",");
         const limit = $('#js-max-results').val();
         getParksList(state, limit);
+        // if (responseJson.total === 0) {
+        //     $('js-error-message').text('Something went wrong')
+        // }
     });
 }
 
